729b234cdb1a6c786f690a9720dd5967a2182030
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-28 11:48:15 +0800

    Use Function and modify the way of finding three max number

--- ThreeNumber.py
+++ ThreeNumber.py
@@ -1,18 +1,33 @@
 import sys
 
-x = []
-with open(sys.argv[1]) as fo:
-    while True:
-        line = fo.readline()
-        if line == "":
-            break
-        line = line.strip()
-        x.append(line)
+def access_from_file(file):
+    x = []
+    with open(sys.argv[1]) as fo:
+        while True:
+            line = fo.readline()
+            if line == "":
+                break
+            line = line.strip()
+            x.append(line)
+    return x
 
-for i in range(3):
-    max = -1
-    for j in x:
-        if j > max:
-        	max = j
-    print max
-    del x[x.index(max)]
+def find_three_max(x):
+
+    #for i in range(3):
+    #    max = -1
+    #    for j in x:
+    #        if j > max:
+    #            max = j
+    #    print max
+    #    del x[x.index(max)]
+
+    ls = [0, 0, 0]
+    for i in x:
+        for index, element in enumerate(ls[:3]):
+            if i > element:
+                ls.insert(index, i)
+                break
+    print ls[:3]
+        
+
+find_three_max(access_from_file(sys.argv[1]))
