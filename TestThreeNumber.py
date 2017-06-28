dbfb0618c46da21922d7b01d208445cefab5cea8
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-28 16:00:32 +0800

    To test ThreeNumber.py is correct, contain three units

--- TestThreeNumber.py
+++ TestThreeNumber.py
@@ -5,7 +5,7 @@ class TestThreeNumber(unittest.TestCase):
 	def test_access_from_file(self):
 		file_name = "arr.dat"
 		expacted_value = ['2134', '3412', '6421', '8723', '9239', '1234', '2345']
-		value = ThreeNumber.access_from_file("arr.dat")
+		value = ThreeNumber.access_from_file(file_name)
 		self.assertEqual(expacted_value, value)
 	def test_find_three_max_1(self):
 		number = [400, 100, 50, 30, 300, 200, 10]
