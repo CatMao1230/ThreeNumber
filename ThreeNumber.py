795e5e75d01e91935251e05655c5999bc6514683
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-28 15:53:00 +0800

    Fix the function access_from_file and find_three_max

--- ThreeNumber.py
+++ ThreeNumber.py
@@ -2,7 +2,7 @@ import sys
 
 def access_from_file(file):
     x = []
-    with open(sys.argv[1]) as fo:
+    with open(file) as fo:
         while True:
             line = fo.readline()
             if line == "":
@@ -13,21 +13,13 @@ def access_from_file(file):
 
 def find_three_max(x):
 
-    #for i in range(3):
-    #    max = -1
-    #    for j in x:
-    #        if j > max:
-    #            max = j
-    #    print max
-    #    del x[x.index(max)]
-
     ls = [0, 0, 0]
     for i in x:
         for index, element in enumerate(ls[:3]):
             if i > element:
                 ls.insert(index, i)
                 break
-    print ls[:3]
-        
+    return ls[:3]
 
-find_three_max(access_from_file(sys.argv[1]))
+if __name__ == '__main__':
+    find_three_max(access_from_file(sys.argv[1]))
